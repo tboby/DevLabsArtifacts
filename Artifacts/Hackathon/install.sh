d959e20ce97b0eba13fb281bde4d10699a0f453f
Thomas Boby <thomas@boby.uk>
2026-05-26 11:30:16 +0100

    Implement logging in install.sh
    Add logging to the install script for NVM.

--- Artifacts/Hackathon/install.sh
+++ Artifacts/Hackathon/install.sh
@@ -11,7 +11,9 @@ Script to install NVM on Linux package on Azure DevTest lab Linux VMs.
 
 #Get the input parameter value.
 TARGET_USER=$1
-
+LOG_FILE="/var/log/linux_install_nvm_v1.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+echo "Started at $(date)"
 # Check if nvm is already installed.
 echo "Checking if NVM is already installed."
 echo "For user $TARGET_USER"
